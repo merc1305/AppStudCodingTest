76929ed527a26e4fe26ead334757dc9514da9c25
Yauheni Shauchenka <yshauchenka@elilink.com>
2017-05-25 12:05:58 +0300

    ADD: base app structure

---
+++ APPSTUD-Test/APPSTUD-Test/Connections/APIConnectionManager.swift
@@ -0,0 +1,9 @@
+//
+//  APIConnectionManager.swift
+//  APPSTUD-Test
+//
+//  Created by Yauheni Shauchenka on 5/25/17.
+//  Copyright © 2017 com.appstub. All rights reserved.
+//
+
+import Foundation
